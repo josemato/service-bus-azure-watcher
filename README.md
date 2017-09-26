f7b50d30318f44f833d48f7faabf6a1cde1b42e8
Jose Mato <jose.mato@accenture.com>
2017-09-26 21:20:44 +0200

    Create library to manage azure messages

---
+++ README.md
@@ -0,0 +1,52 @@
+Service Bus Azure Watcher
+===========================
+
+Small library to help handle Azure Service Bus messages
+
+## Installation
+
+  `npm install @josemato/service-bus-azure-watcher`
+
+## ServiceBusWatcher initialization
+
+Constructor of ```ServiceBusWatcher``` needs:
+* serviceBus: an azure serviceBusService
+* queueName: The name of the queue where to retrieve the messages
+* concurrency: Max simultaneous messages to be handle
+
+## Usage
+```javascript
+const azure = require('azure');
+const ServiceBusAzureWatcher = require('service-bus-azure-watcher');
+
+const AZURE_SERVICE_BUS_KEY = 'YOUR_SERVICE_BUS_CONNECTION_STRING';
+const QUEUE_NAME = 'YOUR_QUEUE_NAME';
+const CONCURRENCY = 50;
+
+const retryOperations = new azure.ExponentialRetryPolicyFilter();
+const serviceBus = azure.createServiceBusService(AZURE_SERVICE_BUS_KEY).withFilter(retryOperations);
+const myServiceBusWather = new ServiceBusAzureWatcher(serviceBus, QUEUE_NAME, CONCURRENCY);
+
+/**
+* User function that process a message. When finish, it's necessary to notify "done" function.
+* This "done" function allow one error parameter to indicate user operation failed.
+* @param {Object} message Azure message
+* @param {Function} done callback function to notify service bus
+*  watcher that user finished to process the message
+* */
+myServiceBusWather.onMessage((message, done) => {
+    console.log('received message', message.body);
+    done();
+    // if user operation failed, you need to call done('some problem') or done(new Error('some problem'))
+});
+
+/**
+* @param {Object} err This error is an instace of ErrorMessage:
+*  stack, error message, status and queueMessage (undefined by default) are available
+* */
+myServiceBusWather.onError((err) => {
+    console.log('user function onError', err.message, err.status, err.queueMessage);
+});
+
+myServiceBusWather.start();
+````
---
+++ index.js
@@ -0,0 +1,339 @@
+'use strict';
+
+const EventEmitter = require('events');
+const promiseRetry = require('promise-retry');
+
+const CHECK_CONCURRENCY_TIMER = 10000;
+
+/**
+ * Class to be used on any emitter error to pass a reason and the current message
+ */
+class ErrorMessage extends Error {
+  constructor(message, status, queueMessage) {
+    super(message);
+    this.status = status;
+    this.queueMessage = queueMessage;
+
+    if (typeof Error.captureStackTrace === 'function') {
+      Error.captureStackTrace(this, this.constructor);
+    } else {
+      this.stack = (new Error(message)).stack;
+    }
+  }
+}
+
+class DebugMessage {
+  /**
+   *
+   * @param {String} message
+   * @param {Object} metadata
+   */
+  constructor(message, metadata = null) {
+    this.message = message;
+    this.metadata = metadata;
+  }
+}
+
+/**
+ * Event errors that can be emitted this library
+ */
+
+const ON_ERROR_READ_MESSAGE_FROM_AZURE = 'on_error_read_message_from_azure';
+const QUEUE_NOT_FOUND = 'queue_not_found';
+const ON_UNLOCK_MESSAGE_FROM_AZURE = 'on_unlock_message_from_azure';
+const ON_UNLOCK_MESSAGE_FROM_AZURE_MAX_ATTEMPTS = 'on_unlock_message_from_azure_max_attempts';
+const ON_DELETE_MESSAGE_FROM_AZURE_MAX_ATTEMPTS = 'on_delete_message_from_azure_max_attempts';
+
+/**
+ * Operational errors (to be used with ErrorMessage)
+ */
+const AZURE_NO_MESSAGES = 'No messages to receive';
+
+/**
+ * The options argument is an object which maps to the retry module options:
+ * retries: The maximum amount of times to retry the operation. Default is 10.
+ * factor: The exponential factor to use. Default is 2.
+ * minTimeout: The number of milliseconds before starting the first retry. Default is 1000.
+ * maxTimeout: The maximum number of milliseconds between two retries. Default is Infinity.
+ * randomize: Randomizes the timeouts by multiplying with a factor between 1 to 2. Default is false.
+ */
+const optionRetryPromise = {
+  retries: 3,
+  maxTimeout: 4000,
+};
+
+/**
+ * Private methods to be used inside this module
+ *  getQueueData
+ */
+class ServiceBusPrivate {
+  constructor(azureServiceBus) {
+    this.serviceBus = azureServiceBus;
+
+    this.getQueueData = this.getQueueData.bind(this);
+    this.unlockMessage = this.unlockMessage.bind(this);
+    this.removeMessage = this.removeMessage.bind(this);
+  }
+
+  /**
+   * @param {Object} azureBus
+   * @param {String} queueName
+   * @result {Object} queueData or null
+   */
+  getQueueData(queueName) {
+    return new Promise((resolve, reject) => {
+      this.serviceBus.listQueues((err, remoteQueues) => {
+        if (err) {
+          return reject(err);
+        }
+
+        if (!Array.isArray(remoteQueues)) {
+          return reject(`remoteQueues is not an array: ${remoteQueues}`);
+        }
+
+        const myQueues = remoteQueues.filter((queue) => {
+          return queue.QueueName === queueName;
+        });
+
+        const myQueue = myQueues.length !== 0 ? myQueues[0] : null;
+
+        return resolve(myQueue);
+      });
+    });
+  }
+
+  unlockMessage(message) {
+    return new Promise((resolve, reject) => {
+      this.serviceBus.unlockMessage(message, (err, data) => {
+        if (err) {
+          return reject(err);
+        }
+
+        return resolve(data);
+      });
+    });
+  }
+
+  removeMessage(message) {
+    return new Promise((resolve, reject) => {
+      this.serviceBus.deleteMessage(message, (err, data) => {
+        if (err) {
+          return reject(err);
+        }
+
+        return resolve(data);
+      });
+    });
+  }
+}
+
+class ServiceBusAzureWatcher {
+  constructor(azureServiceBus, queueName, concurrency = 1) {
+    this.serviceBus = azureServiceBus;
+    this.queueName = queueName;
+    this.concurrency = concurrency;
+    this.onMessageCallback = () => {};
+
+    this.queueData = null;
+    this.myEmitter = new EventEmitter();
+    this.sbPrivate = new ServiceBusPrivate(this.serviceBus);
+
+    /**
+     * state variables to know what action to do in checkConcurrency
+     */
+    this.isStartRunning = false;
+    this.maxThreadsCreated = 0;
+
+    // bind methods
+    this.start = this.start.bind(this);
+    this.startReceivingMessages = this.startReceivingMessages.bind(this);
+    this.checkConcurrency = this.checkConcurrency.bind(this);
+    this.readOneMessage = this.readOneMessage.bind(this);
+    this.onMessage = this.onMessage.bind(this);
+    this.onError = this.onError.bind(this);
+    this.onDebug = this.onDebug.bind(this);
+  }
+
+  /**
+   * Starts reading pool messages from azure
+   * 1. Check if queue exist
+   * 2. Start receiving messages
+   */
+  start() {
+    this.sbPrivate.getQueueData(this.queueName).then((queueData) => {
+      if (queueData === null) {
+        return this.myEmitter.emit('error', new ErrorMessage(`queue not found: ${this.queueName}`, QUEUE_NOT_FOUND, null));
+      }
+
+      this.queueData = queueData;
+
+      this.startReceivingMessages();
+
+      setTimeout(() => {
+        this.myEmitter.emit('debug', new DebugMessage('starting concurrency checker'));
+        this.checkConcurrency();
+      }, CHECK_CONCURRENCY_TIMER);
+    }).catch((err) => {
+      this.myEmitter.emit('error', 'start_error', new ErrorMessage(err, 'start_error', null));
+    });
+  }
+
+  /**
+   * Special method that must be run each X seconds to check if we have enough messages
+   * to enable the multi option process (call readOneMessage more than once after), scenario:
+   *  1. nodejs run and there is 0 messages in queue
+   *  2. node js just run one process to check if there are messages and pass it to user callback
+   *  3. then, there are a lot of messages in queue
+   *  4. so, it's necessary create max CONCURRENCY process to attendee that requests
+   */
+  checkConcurrency() {
+    if (this.queueData) {
+      this.sbPrivate.getQueueData(this.queueName).then((queueData) => {
+        if (queueData === null) {
+          setTimeout(() => { this.checkConcurrency(); }, CHECK_CONCURRENCY_TIMER);
+          return this.myEmitter.emit('error', new ErrorMessage(`queue not found: ${this.queueName}`, QUEUE_NOT_FOUND, null));
+        }
+
+        const currentMessagesInQueue = parseInt(queueData.CountDetails['d2p1:ActiveMessageCount'], 10) || 0;
+        if (currentMessagesInQueue === 0) {
+          setTimeout(() => { this.checkConcurrency(); }, CHECK_CONCURRENCY_TIMER);
+          this.maxThreadsCreated = 1;
+
+          return null;
+        }
+
+        if (currentMessagesInQueue > this.concurrency && this.maxThreadsCreated < this.concurrency) {
+          while (this.maxThreadsCreated < this.concurrency) {
+            this.maxThreadsCreated = this.maxThreadsCreated + 1;
+            this.readOneMessage();
+          }
+        } else if (currentMessagesInQueue < this.maxThreadsCreated) {
+          this.maxThreadsCreated = currentMessagesInQueue;
+        }
+
+        setTimeout(() => { this.checkConcurrency(); }, CHECK_CONCURRENCY_TIMER);
+      }).catch((err) => {
+        this.myEmitter.emit('error', 'start_error', new ErrorMessage(err, 'start_error', null));
+
+        setTimeout(() => { this.checkConcurrency(); }, CHECK_CONCURRENCY_TIMER);
+      });
+    }
+  }
+
+  /**
+   * Start reading max concurrency messages but putting a small delay
+   * to avoid network bottle necks in azure
+   */
+  startReceivingMessages() {
+    /**
+     * Variables to keep a small delay between request to avoid network bottlenecks
+     */
+    let delay = 0;
+    let i = 0;
+    this.isStartRunning = true;
+    this.maxThreadsCreated = 0;
+
+    /**
+     * If there are no messages in the queue, we tried to read just one message
+     * becasue readOneMessage function has a mechanism to lock for new messages
+     * each X time
+     * 'd2p1:ActiveMessageCount' is the pending messages to be deliver in the queue
+     * (no the number of dead letters)
+     */
+    const currentMessagesInQueue = parseInt(this.queueData.CountDetails['d2p1:ActiveMessageCount'], 10) || 0;
+
+    if (currentMessagesInQueue === 0) {
+      this.maxThreadsCreated = this.maxThreadsCreated + 1;
+      return this.readOneMessage();
+    }
+
+    /**
+     * If there are messages, try to read them keeping concurrency and
+     * dont create more requests than messages in the queue
+     */
+    let maxCallsInvoked = -1;
+    while (++ maxCallsInvoked < this.concurrency && maxCallsInvoked < currentMessagesInQueue) {
+      this.maxThreadsCreated = this.maxThreadsCreated + 1;
+      delay = i + 25;
+
+      setTimeout(() => {
+        this.readOneMessage();
+      }, delay);
+    }
+
+    this.isStartRunning = false;
+  }
+
+  readOneMessage() {
+    if (this.maxThreadsCreated > this.concurrency) {
+      this.maxThreadsCreated = this.concurrency;
+      return;
+    }
+
+    this.serviceBus.receiveQueueMessage(this.queueName, { isPeekLock: true }, (err, message) => {
+      if (err === AZURE_NO_MESSAGES) {
+        setTimeout(() => {
+          return this.readOneMessage();
+        }, 500);
+        return;
+      }
+
+      if (err) {
+        this.myEmitter.emit('error', new ErrorMessage(err, ON_ERROR_READ_MESSAGE_FROM_AZURE, message));
+        return null;
+      }
+
+      // we can pass data to user
+      const done = ((originalMessage) => {
+        return (err) => {
+          if (err) {
+            return promiseRetry((retry, attempts) => {
+              this.myEmitter.emit('debug', new DebugMessage('retry unlockMessage'));
+              return this.sbPrivate.unlockMessage(originalMessage).catch((err) => {
+                this.myEmitter.emit('error', new ErrorMessage(err, ON_UNLOCK_MESSAGE_FROM_AZURE, message));
+                retry(err);
+              });
+            }, optionRetryPromise).then(() => {
+              this.readOneMessage();
+            }, (err) => {
+              this.myEmitter.emit('error', new ErrorMessage(err, ON_UNLOCK_MESSAGE_FROM_AZURE_MAX_ATTEMPTS, message));
+              this.readOneMessage();
+            });
+          }
+
+          return promiseRetry((retry, attempts) => {
+            this.sbPrivate.removeMessage(originalMessage).then((data) => {
+              this.myEmitter.emit('debug', new DebugMessage('messageDeleted', originalMessage));
+              this.readOneMessage();
+            });
+          }, optionRetryPromise).then(() => {
+            this.readOneMessage();
+          }, (err) => {
+            this.myEmitter.emit('error', new ErrorMessage(err, ON_DELETE_MESSAGE_FROM_AZURE_MAX_ATTEMPTS, message));
+            this.readOneMessage();
+          });
+        };
+      })(message);
+
+      this.onMessageCallback(message, done);
+    });
+  }
+
+  /**
+   * Mehtod to be invoked after a message received
+   * @param {Function} callback
+   */
+  onMessage(callback) {
+    this.onMessageCallback = callback;
+  }
+
+  onError(callback) {
+    this.myEmitter.on('error', callback);
+  }
+
+  onDebug(callback) {
+    this.myEmitter.on('debug', callback);
+  }
+}
+
+module.exports = ServiceBusAzureWatcher;
